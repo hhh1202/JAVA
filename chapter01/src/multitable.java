081bffaff8868dcfe922e53c39ca0adfd2756eca
ghksgml6068-debug <ghksgml6068@gmail.com>
2026-01-14 16:48:08 +0900

    Java 연습

---
+++ chapter01/src/multitable.java
@@ -0,0 +1,15 @@
+public class multitable {
+    public static void main(String[] args){
+        for(int i=0; i<10; i++) {
+            System.out.println(
+                    ((n == 0) ?
+                    String.format("%d단 출력         %d단 출력         %d단 출력", i+2, i+3, i+4)
+                    : String.format("%d * %d = %d")
+            )
+            );
+            for(int a=1; a<=9; a++){
+
+            }
+        }
+    }
+}
