d114a2752d72c089c53b5960f4933fb45357683c
ghksgml6068-debug <ghksgml6068@gmail.com>
2026-01-15 09:37:55 +0900

    0114 구구단 예제

--- chapter01/src/multitable.java
+++ chapter01/src/multitable.java
@@ -1,15 +1,22 @@
 public class multitable {
-    public static void main(String[] args){
-        for(int i=0; i<10; i++) {
-            System.out.println(
-                    ((n == 0) ?
-                    String.format("%d단 출력         %d단 출력         %d단 출력", i+2, i+3, i+4)
-                    : String.format("%d * %d = %d")
-            )
-            );
-            for(int a=1; a<=9; a++){
-
+    public static void main(String[] args) {
+        for (int digit = 2; digit < 9; digit += 3) {
+            for (int n = 0; n < 10; n++) {
+                System.out.println(
+                        (
+                                (n == 0)
+                                        ? String.format("%d단 출력       %d단 출력       %d단 출력", digit, digit + 1, digit + 2)
+                                        : ((digit < 8) ? String.format("%d * %d = %d     %d * %d = %d     %d * %d = %d"
+                                        , digit, n, digit * n
+                                        , digit + 1, n, (digit + 1) * n
+                                        , digit + 2, n, (digit + 2) * n)
+                                        : String.format("%d * %d = %d     %d * %d = %d"
+                                        , digit, n, digit * n
+                                        , digit + 1, n, (digit + 1) * n))
+                        )
+                );
             }
+            System.out.println("");
         }
     }
 }
