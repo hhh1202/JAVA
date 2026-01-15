c7a8aa397db58b14ff27f7d07d14f3d665bf7b86
ghksgml6068-debug <ghksgml6068@gmail.com>
2026-01-15 12:04:20 +0900

    chapter01

---
+++ chapter01/src/test_0115.java
@@ -0,0 +1,8 @@
+public class test_0115 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        for (String item : args){
+            System.out.println(item);
+        }
+    }
+}
