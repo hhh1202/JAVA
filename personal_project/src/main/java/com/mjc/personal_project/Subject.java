3f40adf1d28e25ac53345b30b0d1ac60e055d73f
한환희 <ghksgml6068@gmail.com>
2026-01-28 17:12:08 +0900

    개인과제 수정

--- personal_project/src/main/java/com/mjc/personal_project/Subject.java
+++ personal_project/src/main/java/com/mjc/personal_project/Subject.java
@@ -3,12 +3,13 @@ package com.mjc.personal_project;
 import java.util.Arrays;
 
 public class Subject extends Student{
-    String list[] = new String[10];
-    int count = 0, score, choice, subject_choice;
-    String subject;
+//    String list[] = new String[10];
+    int count = 1, score, choice;
+    String subject, subject_choice;
 
     void calculator() {
         while(true) {
+            String list[] = new String[count];
             System.out.print("1.수강 과목 입력 | 2.성적입력 | 3.종료 ");
             System.out.print("\n선택 : ");
             choice = sc.nextInt();
@@ -17,18 +18,15 @@ public class Subject extends Student{
             if(choice == 1){
                 System.out.print("과목을 입력하세요 : ");
                 subject = sc.nextLine();
-                this.list[count] = subject;
+                list[count] = subject;
                 count++;
             }
             else if(choice == 2){
-                for(int i=0; i<=list.length; i++){
                     System.out.println(Arrays.toString(list));
-                    System.out.println("성적을 입력할 과목을 선택하세요 : ");
-                    subject_choice = sc.nextInt();
-                    System.out.println("성적을 입력하세요 : ");
+                    System.out.print("성적을 입력할 과목을 선택하세요 : ");
+                    subject_choice = sc.nextLine();
+                    System.out.print("성적을 입력하세요 : ");
                     score = sc.nextInt();
-                }
-
             }else if(choice == 3) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
