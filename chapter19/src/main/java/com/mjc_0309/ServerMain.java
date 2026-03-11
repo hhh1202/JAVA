53958e0134c5a6d8f9081af338928a8d48a43a8b
한환희 <ghksgml6068@gmail.com>
2026-03-11 19:58:19 +0900

    19장 예제 연습 서버통신

--- chapter19/src/main/java/com/mjc_0309/ServerMain.java
+++ chapter19/src/main/java/com/mjc_0309/ServerMain.java
@@ -1,9 +1,7 @@
 package com.mjc_0309;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-
-    }
+public class ServerMain {
 }
--- chapter19/src/main/java/com/mjc_0309/test19_3/ClientExample.java
+++ chapter19/src/main/java/com/mjc_0309/test19_3/ClientExample.java
@@ -1,22 +1,45 @@
 package com.mjc_0309.test19_3;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class ClientExample {
-    public static void main(String[] args) {
+    public void run() {
+        Socket socket = null;
+        Scanner scanner = null;
         try {
-            Socket socket = new Socket("localhost", 50001);
-
+            //내 IP : 10.11.83.62
+            socket = new Socket("172.111.117.14",50001);
             System.out.println("[클라이언트] 연결 성공");
 
-            socket.close();
-            System.out.println("[클라이언트 연결 끊음]");
-        } catch (UnknownHostException e) {
+            scanner = new Scanner(System.in);
+
+            String msg = scanner.nextLine();
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bw.write(msg);
+            bw.flush();
 
         } catch (IOException e) {
-
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                socket.close();
+            } catch (Exception e) {
+            }
+            System.out.println("[클라이언트] 연결 끊음");
+            try {
+                scanner.close();
+            } catch (Exception e) {
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        ClientExample ce = new ClientExample();
+        ce.run();
     }
 }
