7e3f5205982a3e54c89daba5ed49f1ea6082966a
한환희 <ghksgml6068@gmail.com>
2026-03-13 18:05:41 +0900

    스레드 개인과제 2번

--- chapter14/src/main/java/com/mjc_0312/Main.java
+++ chapter14/src/main/java/com/mjc_0312/Main.java
@@ -4,6 +4,31 @@ package com.mjc_0312;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Thread mainthread = new Thread() {
+            public void run() {
+                for (int i = 2; i < 5; i++) {
+                    for (int j = 1; j < 10; j++) {
+                        System.out.println(i + " * " + j + " = " + i * j);
+                    }
+                }
+            }
+        };
+        mainthread.start();
 
+        Thread threadA = new Thread() {
+            public void run() {
+                for(int i=5; i<10; i++) {
+                    for(int j=1; j<10; j++) {
+                        System.out.println(i + " * " + j + " = " + i*j);
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e){
+                            throw new RuntimeException();
+                        }
+                    }
+                }
+            }
+        };
+        threadA.start();
     }
 }
