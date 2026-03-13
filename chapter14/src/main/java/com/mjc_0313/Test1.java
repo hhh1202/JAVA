0524fc2b87d31fbe3432c6a311db8af21d159873
한환희 <ghksgml6068@gmail.com>
2026-03-13 17:52:22 +0900

    스레드 개인과제 1 풀이

--- chapter14/src/main/java/com/mjc_0313/Test1.java
+++ chapter14/src/main/java/com/mjc_0313/Test1.java
@@ -1,7 +1,18 @@
 package com.mjc_0313;
 
-public class Test1 extends Thread {
-    private int sum;
+public class Test1 {
+    public static void main(String[] args) {
 
-    
+        for (int i = 2; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.println(i + " * " + j + " = " + i * j);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 }
+
