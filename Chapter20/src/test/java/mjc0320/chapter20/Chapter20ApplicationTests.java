3c7f1dd938c42c10f6ac304cf98a9db8af025387
한환희 <ghksgml6068@gmail.com>
2026-03-20 20:43:13 +0900

    스프링부트를 이용한 웹서버 연결(인텔리제이)

---
+++ Chapter20/src/test/java/mjc0320/chapter20/Chapter20ApplicationTests.java
@@ -0,0 +1,13 @@
+package mjc0320.chapter20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
