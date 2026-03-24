a952f6f9d89acdb3734f2a49188eceb5f110a122
한환희 <ghksgml6068@gmail.com>
2026-03-24 20:55:17 +0900

    데이터베이스 연결 및 저장 기능 추가(미완성)

--- Chapter20/src/main/java/mjc0320/chapter20/gamecrud/GameRestController.java
+++ Chapter20/src/main/java/mjc0320/chapter20/gamecrud/GameRestController.java
@@ -1,11 +1,15 @@
 package mjc0320.chapter20.gamecrud;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GameRestController {
+
+    @Autowired
+    private GameService gameService;
 
     @PostMapping("/api/insert-data")
     public void insertData(@RequestBody GameDto gameDto) {
---
+++ Chapter20/src/main/java/mjc0320/chapter20/gamecrud/IGameMybatis.java
@@ -0,0 +1,8 @@
+package mjc0320.chapter20.gamecrud;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface IGameMybatis {
+    void insertData(GameDto newGame);
+}
